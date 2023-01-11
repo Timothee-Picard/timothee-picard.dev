280ea76e2a63a583ad1f3de27a9a95b59e8c5745
Timothee Picard <timothee.picard@outlook.fr>
2023-01-11 16:40:27 +0100

    change viewport width

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
     app: {
         head: {
             charset: 'utf-16',
-            viewport: 'width=500, initial-scale=1',
+            viewport: 'width=device-width, initial-scale=1',
             title: 'timothee-picard.dev',
             meta: [
                 { name: 'description', content: 'Portfolio de timothée picard développeur web frontend' }
